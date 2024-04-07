4c16783145cc7ef8165f35ea247bd455bf2a7edc
עמנואל ימיני <ImanuelY@moch.gov.il>
2024-04-07 11:01:51 +0300

    test

--- src/app/calc/calc.ts
+++ src/app/calc/calc.ts
@@ -147,8 +147,7 @@ export class Calc implements OnInit {
         window.location.reload();
     }
 }
-
-
+//
 //https://primeng.org/inputtext
 //ng build --output-path docs --base-href /CalcTargetPriceApartment/
 //https://imanuel300.github.io/CalcTargetPriceApartment/
